f2844640afd43464cd35e575790a33cf025989ad
b0rb44 <dennerdeborba@gmail.com>
2023-11-29 17:02:14 -0300

    add app e script.js

--- app.js
+++ app.js
@@ -4,10 +4,54 @@ const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const usuarios = []
+app.use(express.json())
+
+app.post('/cadastro', (req, res) => {
+  for(let i = 0; i < usuarios.length; i++){
+    if(usuarios[i].email == req.body.email){
+        res.status(406).send("Este usuário já foi cadastrado!");
+        return
+    }
+  }
+
+  let hash = bcrypt.hashSync(req.body.senha, 10)
+  let obj = {
+    id: usuarios.length + 1,
+    nome: req.body.nome,
+    email: req.body.email,
+    senha: hash
+  }
+  usuarios.push(obj)
+  res.status(201).send(obj)
+
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.post('/login', (req, res) => {
+  let achou = false
+  let token = ''
+  for(let i = 0; i < usuarios.length; i++){
+      if(usuarios[i].email == req.body.email){
+         achou = bcrypt.compareSync(req.body.senha,usuarios[i].senha)  
+         token = jwt.sign(
+          {
+              id:usuarios[i].id, 
+              nome:usuarios[i].nome,
+              email:usuarios[i].email,
+          })  
+         break    
+      }
+  }
+
+  if(!achou){
+      res.status(404).send("Usuário ou senha inválido")
+      return
+  }
+  
+  res.status(200).send(token)
 })
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-const nome = document.getElementById('inputNome')
-const email = document.getElementById('inputEmail')
-const senha = document.getElementById('inputSenha')
-const confirmSenha = document.getElementById('inputConfirm')
+document.getElementById('formCadastro').addEventListener('submit', async function (event) {
+  event.preventDefault();
 
+  const nome = document.getElementById('inputNome').value
+  const email = document.getElementById('inputEmail').value
+  const senha = document.getElementById('inputSenha').value
+  const confirmSenha = document.getElementById('inputConfirm').value
+
+  if (senha !== confirmSenha) {
+    alert('As senhas não são iguais!')
+    return
+  }
+  try{
+  const response = await fetch('http://localhost:3000/cadastro', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ nome, email, senha }),
+  })
+
+  const data = await response.json()
+  console.log(data)
+  } catch (error){
+    console.error('Erro:', error)
+  }
+})
+
+document.getElementById('formLogin').addEventListener('submit', async function (event) {
+  event.preventDefault();
+
+  const email = document.getElementById('inputEmail').value;
+  const senha = document.getElementById('inputSenha').value;
+
+try{
+  const response = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, senha }),
+  });
+
+  const data = await response.json();
+  console.log(data)
+
+  localStorage.setItem('token', data.token);
+} catch (error){
+  console.error('Erro:', error)
+}
+});
